b648ce528e0586d31d632e31fc09e76879a7d2e2
Anggar Wahyu <anggarwahyu@gmail.com>
2020-04-14 22:26:42 +0700

    Fix bug in test script

--- tugas7/test.sh
+++ tugas7/test.sh
@@ -2,8 +2,8 @@ for c in 1 5 10
 do
     echo "Test for 10 requests with " $c " concurrency"
     ab -n 10 -c $c http://127.0.0.1:10001/ | \
-    grep "Time taken\|Complete req\|Failed req\|Requests per\
-            \|Time per\|Transfer rate"
+    grep "Time taken\|Complete req\|Failed req\|Total trans
+            \|Requests per\|Time per\|Transfer rate"
     echo
 done
 
@@ -11,8 +11,8 @@ for c in 1 10 30 50
 do
     echo "Test for 50 requests with " $c " concurrency"
     ab -n 50 -c $c http://127.0.0.1:10001/ | \
-    grep "Time taken\|Complete req\|Failed req\|Requests per\
-            \|Time per\|Transfer rate"
+    grep "Time taken\|Complete req\|Failed req\|Total trans
+            \|Requests per\|Time per\|Transfer rate"
     echo
 done
 
@@ -20,7 +20,7 @@ for c in 1 10 50 100
 do
     echo "Test for 100 requests with " $c " concurrency"
     ab -n 100 -c $c http://127.0.0.1:10001/ | \
-    grep "Time taken\|Complete req\|Failed req\|Requests per\
-            \|Time per\|Transfer rate"
+    grep "Time taken\|Complete req\|Failed req\|Total trans
+            \|Requests per\|Time per\|Transfer rate"
     echo            
 done
